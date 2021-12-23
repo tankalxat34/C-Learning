f3c1a13855b3dddb45cde2da1c1567a8f123d8f9
tankalxat34 <tankalxat34@gmail.com>
2021-12-23 18:53:28 +0300

    Изменены файлы

--- C++ Learning.cpp
+++ C++ Learning.cpp
@@ -4,16 +4,7 @@ using namespace std;
 
 void main()
 {
-	srand(time(NULL));
-
-	// system("pause");
-
-	// cout << "Hello world!" << endl;
-
-	for (int i = 0; i < 10; i++)
-	{
-		cout << rand() % 10 + 5 << endl;
-	};
-	// system("explorer.exe C:\\Users\\podst\\OneDrive\\Рабочий стол\\РАНХиГС\\РАНХиГС Литература и Лекции\\Логика\\27.10.2021\\");
+	setlocale(0, "Rus");
+	cout << "Привет!" << endl;
 }
 
