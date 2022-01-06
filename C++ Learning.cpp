094590fdf42fd3acf1d95e4b8240a3adb1662432
tankalxat34 <tankalxat34@gmail.com>
2022-01-06 19:41:28 +0300

    Постфиксный и префиксный инкремент и декремент - урок 9

--- C++ Learning.cpp
+++ C++ Learning.cpp
@@ -3,13 +3,9 @@ using namespace std;
 
 void main()
 {
-	setlocale(LC_ALL, "ru");
-	int a, b, c;
+	int a = 1;
+	cout << a << endl;
+	
+	cout << a++ << endl;
 
-	cout << "Введите три числа: " << endl;
-	cin >> a >> b >> c;
-
-	cout << "Сумма: " << a + b + c << endl;
-	cout << "Произведение: " << a * b * c << endl;
-	cout << "Среднее: " << (double)(a + b + c)/3 << endl;
 }
