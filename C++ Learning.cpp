79631f0850de8a978ef30639ed412a7234a7a781
tankalxat34 <tankalxat34@gmail.com>
2022-01-06 18:09:35 +0300

    Изучаю переменные - урок 5

--- C++ Learning.cpp
+++ C++ Learning.cpp
@@ -4,30 +4,6 @@ using namespace std;
 
 void main()
 {
-	setlocale(LC_ALL, "Rus");
-
-	int WIDTH = 0;
-	int HEIGHT = 0;
-
-	cout << "Введите длину: ";
-	cin >> WIDTH;
-	
-	cout << "Введите высоту: ";
-	cin >> HEIGHT;
-
-	for (int j = 0; j < HEIGHT; j++)
-	{
-		for (int i = 0; i < WIDTH; i++)
-		{
-			if (j == 0 or j == HEIGHT - 1 or i == 0 or i == WIDTH - 1)
-			{
-				cout << "*";
-			}
-			else
-			{
-				cout << " ";
-			};
-		};
-		cout << endl;
-	};	
+	setlocale(LC_ALL, "ru");
+	int a;
 }
