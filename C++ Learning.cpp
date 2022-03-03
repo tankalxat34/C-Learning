807c2450956d4efe7c619f2a5b74b12b32ff8782
tankalxat34 <tankalxat34@gmail.com>
2022-03-03 10:16:23 +0300

    Использованы ANSI последовательности

--- C++ Learning.cpp
+++ C++ Learning.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
-using namespace std;
 
+using namespace std;
 
 /* 
 №1 - 1. Разработать программу, которая выводит на экран линию из
@@ -13,36 +13,5 @@ void main()
 {
 	setlocale(0, "ru");
 	
-	int count_of_symbols;
-	int counter = 0;
-	char symbol;
-	int orient;
-
-	cout << "Данная программа выводит линию из символов на экран" << endl;
-
-	cout << "Введите длину линии>>> ";
-	cin >> count_of_symbols;
-
-	cout << "Введите символ>>> ";
-	cin >> symbol;
-
-	cout << "Введите ориентацию\n\t1. горизонтальная\n\t2. вертикальная\n\n>>> ";
-	cin >> orient;
-
-	while (counter < count_of_symbols)
-	{
-		switch (orient)
-		{
-		case 1:
-			cout << symbol;
-			break;
-		case 2:
-			cout << symbol << endl;
-			break;
-		default:
-			cout << "Введено неверное значение для ориентации. Программа завершена." << endl;
-			break;
-		}
-		counter++;
-	}
+	cout << "\033[92mПривет мир!\033[0m" << endl;
 }
