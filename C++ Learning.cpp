50ac24b1dca34b837dae723623a163dcfedc6d99
tankalxat34 <tankalxat34@gmail.com>
2022-06-25 11:16:50 +0300

    Прошел урок про break

--- C++ Learning.cpp
+++ C++ Learning.cpp
@@ -1,24 +1,30 @@
 ﻿#include <iostream>
-#include "paintprint.h"
-
 using namespace std;
-
-/* 
-№1 - 1. Разработать программу, которая выводит на экран линию из
-символов. Число символов, какой использовать символ, и какая будет
-линия - вертикальная, или горизонтальная - указывает пользователь.
-*/
-
 
 void main()
 {
 	setlocale(0, "ru");
-	srand(time(NULL));
 	
-	cout << "\033[92m" << "Привет мир!" << "\033[0m" << endl;
+	cout << "начало цикла" << endl;
 
-	for (int i = 0; i < 10; i++)
-	{
-		cout << rand() % 10 << endl;
-	};
+	for (int i = 0; ; i++) {
+		cout << "переменная i = " << i << endl;
+		if (i == 20) {
+			break;
+		}
+	}
+
+	cout << "конец цикла" << endl;
+
+
+	int i = 0;
+
+	while (true) {
+		cout << "i = " << ++i << endl;
+		if (i == 6) {
+			break;
+		}
+	}
+
+
 }
