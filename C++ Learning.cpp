d93dda06a97bae09ab7a92be37da13d546700fca
tankalxat34 <tankalxat34@gmail.com>
2022-06-26 17:39:19 +0300

    Сделал ДЗ + поиск минимума

--- C++ Learning.cpp
+++ C++ Learning.cpp
@@ -1,15 +1,42 @@
 ﻿#include <iostream>
+#include <ctime>
 using namespace std;
 
 void main()
 {
 	setlocale(0, "ru");
 
-	int arr[]{ 5, 33, 545, 111, 326 };
+	srand(time(NULL));
 
-	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
-	{
-		cout << arr[i] << endl;
+	const int SIZE = 10;
+	int arr[SIZE];
+
+	int a;
+
+	for (int i = 0; i < SIZE; i++) {
+		a = rand() % 20;
+		for (int j = 0; j < i; ) {
+			if (a == arr[j]) {
+				a = rand() % 20;
+				j = 0;
+			}
+			else {
+				j++;
+			}
+		}
+		arr[i] = a;
 	}
 
+	for (int i = 0; i < SIZE; i++) {
+		cout << arr[i] << endl;
+	}
+	cout << "************************" << endl;
+
+	int minDigit = arr[0];
+	for (int i = 1; i < SIZE; i++) {
+		if (arr[i] < minDigit) {
+			minDigit = arr[i];
+		}
+	}
+	cout << "Минимальное число: " << minDigit << endl;
 }
